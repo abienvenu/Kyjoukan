56fb3061da486636276d976588f66748865c1dda
Arnaud Bienvenu <abi@abienvenu.net>
2016-04-25 23:38:58 +0200

    added EventController

---
+++ Controller/EventController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Abienvenu\KyjoukanBundle\Controller;
+
+use Abienvenu\KyjoukanBundle\Entity\Event;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class EventController extends Controller
+{
+
+	/**
+	 * @Route("/{slug}");
+	 * @param Event $event
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function indexAction(Event $event)
+	{
+		return $this->render("KyjoukanBundle:Event:index.html.twig", ['event' => $event]);
+	}
+}
