c0c6f5f90b507776186fc07588b7ef7c9ac70672
Arnaud Bienvenu <abi@abienvenu.net>
2016-04-27 23:18:16 +0200

    added flash message and shuffle (mock) action

--- Controller/PhaseController.php
+++ Controller/PhaseController.php
@@ -2,23 +2,34 @@
 
 namespace Abienvenu\KyjoukanBundle\Controller;
 
-use Abienvenu\KyjoukanBundle\Entity\Event;
 use Abienvenu\KyjoukanBundle\Entity\Phase;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+* @Route("/event/{slug_event}/phase");
+*/
 
 class PhaseController extends Controller
 {
 	/**
-	 * @Route("/event/{slug_event}/phase/{slug_phase}");
-	 * @ParamConverter("event", options={"mapping": {"slug_event": "slug"}})
-     * @ParamConverter("phase", options={"mapping": {"slug_phase": "slug"}})
+	 * @Route("/{slug}");
 	 * @param Phase $phase
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-	public function indexAction(Event $event, Phase $phase)
+	public function indexAction(Phase $phase)
 	{
 		return $this->render("KyjoukanBundle:Phase:index.html.twig", ['phase' => $phase]);
+	}
+
+	/**
+	 * @Route("/{slug}/shuffle")
+	 * @param Phase $phase
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
+	public function shuffleAction(Phase $phase)
+	{
+		$this->addFlash('success', "Shuffle not implemented yet.");
+		return $this->redirectToRoute("abienvenu_kyjoukan_phase_index", ['slug_event' => $phase->getEvent()->getSlug(), 'slug' => $phase->getSlug()]);
 	}
 }
