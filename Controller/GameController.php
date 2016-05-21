940ecd118fccca60d0fb7a80d899505cad78f665
Arnaud Bienvenu <abi@abienvenu.net>
2016-05-21 21:37:24 +0200

    game edit

---
+++ Controller/GameController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Abienvenu\KyjoukanBundle\Controller;
+
+use Abienvenu\KyjoukanBundle\Entity\Game;
+use Abienvenu\KyjoukanBundle\Form\Type\GameType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/game/{id}")
+ */
+class GameController extends Controller
+{
+
+	/**
+	 * Displays a form to edit an existing Team entity.
+	 *
+	 * @Route("/edit")
+	 * @param Request $request
+	 * @param Game $game
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function editAction(Request $request, Game $game)
+	{
+		$form = $this->createForm(new GameType($game->getPool()->getPhase()), $game);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$em->flush();
+
+			return $this->redirect(
+				$this->generateUrl('abienvenu_kyjoukan_phase_index', [
+					'slug_event' => $game->getPool()->getPhase()->getEvent()->getSlug(),
+				    'slug' => $game->getPool()->getPhase()->getSlug(),
+				]) . "#games");
+		}
+
+		return $this->render('KyjoukanBundle:Game:edit.html.twig', ['game' => $game, 'form' => $form->createView()]);
+	}
+}
---
+++ Form/Type/GameType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Abienvenu\KyjoukanBundle\Form\Type;
+
+use Abienvenu\KyjoukanBundle\Entity\Phase;
+use Abienvenu\KyjoukanBundle\Repository\GroundRepository;
+use Abienvenu\KyjoukanBundle\Repository\RoundRepository;
+use Abienvenu\KyjoukanBundle\Repository\TeamRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+	private $phase;
+
+	public function __construct(Phase $phase)
+	{
+		$this->phase = $phase;
+	}
+
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('team1', EntityType::class, ['class' => "KyjoukanBundle:Team", 'property' => 'name', 'label' => "Équipe A"])
+			->add('team2', EntityType::class, ['class' => "KyjoukanBundle:Team", 'property' => 'name', 'label' => "Équipe B"])
+			->add('referee', EntityType::class, [
+				'class' => "KyjoukanBundle:Team",
+				'property' => 'name',
+				'label' => "Arbitre",
+				'query_builder' => function(TeamRepository $repo)
+				{
+					return $repo->getTeamsForPhase($this->phase);
+				}
+			])
+			->add('ground', EntityType::class, [
+				'class' => "KyjoukanBundle:Ground",
+				'property' => 'name',
+				'label' => "Terrain",
+			    'query_builder' => function(GroundRepository $repo)
+					{
+						return $repo->getGroundsForEvent($this->phase->getEvent());
+					}
+			])
+			->add('round', EntityType::class, [
+				'class' => "KyjoukanBundle:Round",
+				'property' => 'number',
+				'label' => "Tour",
+			    'query_builder' => function(RoundRepository $repo)
+					{
+						return $repo->getRoundsForPhase($this->phase);
+					}
+			])
+			->add('score1', IntegerType::class, ['label' => "Score de A", 'required' => false])
+			->add('score2', IntegerType::class, ['label' => "Score de B", 'required' => false])
+		;
+	}
+
+	/**
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(['data_class' => 'Abienvenu\KyjoukanBundle\Entity\Game']);
+	}
+}
