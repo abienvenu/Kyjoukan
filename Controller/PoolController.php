830eb67d8338fb0881af945c3a033f648d5ee36e
Arnaud Bienvenu <abi@abienvenu.net>
2016-05-29 15:22:42 +0200

    added ability to edit pools

--- Controller/PoolController.php
+++ Controller/PoolController.php
@@ -3,18 +3,46 @@
 namespace Abienvenu\KyjoukanBundle\Controller;
 
 use Abienvenu\KyjoukanBundle\Entity\Pool;
+use Abienvenu\KyjoukanBundle\Form\Type\PoolType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/pool")
+ * @Route("/pool/{id}")
  */
 class PoolController extends Controller
 {
 	/**
+	 * Displays a form to edit an existing Pool entity.
+	 *
+	 * @Route("/edit")
+	 * @param Request $request
+	 * @param Pool $pool
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+	 */
+	public function editAction(Request $request, Pool $pool)
+	{
+		$form = $this->createForm(new PoolType($pool->getPhase()), $pool);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid())
+		{
+			$em = $this->getDoctrine()->getManager();
+			$em->flush();
+
+			return $this->redirect($this->generateUrl('abienvenu_kyjoukan_phase_index', [
+				                       'slug_event' => $pool->getPhase()->getEvent()->getSlug(), 'slug' => $pool->getPhase()->getSlug()]) . "#pools");
+		}
+
+		return $this->render('KyjoukanBundle:Pool:edit.html.twig', ['pool' => $pool, 'form' => $form->createView()]);
+	}
+
+
+	/**
 	 * Deletes a Pool entity.
 	 *
-	 * @Route("/delete/{id}")
+	 * @Route("/delete")
 	 * @param Pool $pool
 	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
 	 */
---
+++ Form/Type/PoolType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Abienvenu\KyjoukanBundle\Form\Type;
+
+use Abienvenu\KyjoukanBundle\Entity\Phase;
+use Abienvenu\KyjoukanBundle\Repository\TeamRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PoolType extends AbstractType
+{
+	private $phase;
+
+	public function __construct(Phase $phase)
+	{
+		$this->phase = $phase;
+	}
+
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('teams', EntityType::class, [
+				'label' => "Équipes",
+				'class' => 'Abienvenu\KyjoukanBundle\Entity\Team',
+				'property' => 'name',
+				'multiple' => true,
+				'expanded' => true,
+				'query_builder' => function(TeamRepository $repo)
+				{
+					return $repo->getTeamsForPhase($this->phase);
+				}
+			])
+		;
+	}
+
+	/**
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(['data_class' => 'Abienvenu\KyjoukanBundle\Entity\Pool']);
+	}
+}
